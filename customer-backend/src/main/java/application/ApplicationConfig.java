ab4c1d90c7d60d2167a9564d7fb8373e99775970
sce <sce@GLOB_DEV_PRD009.ju.globaz.ch>
2015-10-27 12:18:42 +0100

    intial commit

---
+++ customer-backend/src/main/java/application/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package application;
+
+import javax.inject.Named;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationConfig {
+
+	@Named
+	static class JerseyConfig extends ResourceConfig {
+		public JerseyConfig() {
+			this.packages("application.rest");
+		}
+	}
+		
+
+}
